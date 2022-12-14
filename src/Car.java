e645cc0fd20fae9b3d74a11df1f9c2fbb65d9af4
BogatyrIrina <kochereva_irina@mail.ru>
2022-12-15 00:57:19 +0300

    Задание 2 (машины) выполнено

---
+++ src/Car.java
@@ -0,0 +1,17 @@
+public class Car {
+    public String brand;
+    public String model;
+    public double engineVolume;
+    public String color;
+    public int productionYear;
+    public String origin;
+
+    public Car(String brand, String model, double engineVolume, String color, int productionYear, String origin) {
+        this.brand = brand;
+        this.model = model;
+        this.engineVolume = engineVolume;
+        this.color = color;
+        this.productionYear = productionYear;
+        this.origin = origin;
+    }
+}
